860389f79032f2499514bad0aff45f6d8f7054ce
Richard Chapman <rcchapman85@gmail.com>
2022-03-25 16:27:05 +0000

    Optimistic UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,5 @@
 import {
   ActionFunction,
-  Form,
-  Link,
   LoaderFunction,
   redirect,
   useCatch,
@@ -9,6 +7,8 @@ import {
   useParams,
 } from 'remix';
 import type { Joke } from '@prisma/client';
+
+import { JokeDisplay } from '~/components/joke';
 
 import { db } from '~/utils/db.server';
 import { getUserId, requireUserId } from '~/utils/session.server';
@@ -103,26 +103,8 @@ export function ErrorBoundary() {
 }
 
 // React
-const JokeId = () => {
+export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-
-      {data.isOwner ? (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
-  );
-};
-
-export default JokeId;
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
+}
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -7,7 +7,9 @@ import {
   redirect,
   useActionData,
   useCatch,
+  useTransition,
 } from 'remix';
+import { JokeDisplay } from '~/components/joke';
 
 import { db } from '~/utils/db.server';
 import { getUserId, requireUserId } from '~/utils/session.server';
@@ -112,6 +114,21 @@ export function ErrorBoundary({ error }: { error: Error }) {
 // React
 const JokesNew = () => {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+
+  if (transition.submission) {
+    const name = transition.submission.formData.get('name');
+    const content = transition.submission.formData.get('content');
+
+    if (
+      typeof name === 'string' &&
+      typeof content === 'string' &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return <JokeDisplay joke={{ name, content }} isOwner={true} canDelete={false} />;
+    }
+  }
 
   return (
     <div>
